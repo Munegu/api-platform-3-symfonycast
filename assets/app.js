0ba297e833806a9ecd8b8aad6d429828dbd4e686
nicolas villetelle <nicolas.villetelle@adveris.fr>
2023-05-26 16:10:03 +0200

    react admin

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+import { registerReactControllerComponents } from '@symfony/ux-react';
+
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
+
